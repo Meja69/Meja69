8bf2dadbdf82aaf4969ca4e3eb1a7ac9b181bccf
Meja69 <152335517+Meja69@users.noreply.github.com>
2023-11-29 00:29:51 +0300

    Create README.m
    Am 20 years of age

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Meja69 am a kenyan
+- 👀 I’m interested in ...working hand in hand with github.co.
+- 🌱 I’m currently learning ...in medical training college taking orthopaedic technology course(nairobi,kenya)
+- 💞️ I’m looking to collaborate on ...walk hand in hand willingly
+- 📫 How to reach me ...You will reach me via my watsap number +254719654862,email mathewkoske@gmail.com
+
+<!---
+Meja69/Meja69 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
